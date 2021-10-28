17abeabaccfd26696c9cea68f9820370580be075
mao28rr <mauricio.restrepo@fundacionview.org>
2021-10-27 20:27:42 -0500

    actualizacion documentos

--- SG-SST-1H/documentos/models.py
+++ SG-SST-1H/documentos/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.db.models.deletion import CASCADE
 from empresa.models import Empresa, Empleados
+from comites.models import Comites
 
 # funcion para subir documentos
 
@@ -13,6 +14,12 @@ def subir_docuEmpleados(instance, filename):
     old_instance = DocuEmpleados.objects.get(pk=instance.pk)
     old_instance.path_documento.delete()
     return 'recursos/docsempleados/' + filename
+
+def subir_docscomite(instance, filename):
+    old_instance = DocsComite.objects.get(pk=instance.pk)
+    old_instance.path_documento.delete()
+    return 'recursos/docscomite/' + filename
+
 
 # Create your models here.
 
@@ -59,6 +66,23 @@ class DocuEmpleados(models.Model):
     class Meta:
         verbose_name = "Nombre documento empleado"
         verbose_name_plural = "Nombre documentos empleados"
+        
+    def __str__(self) -> str:
+        return self.nombre_documento
+
+class DocsComite(models.Model):
+    # atributo de la clase ducumento comite
+    comite = models.ForeignKey(Comites, on_delete=CASCADE)
+    nombre_documento = models.CharField(verbose_name = "nombre del documento", max_length = 50, null = False)
+    path_documento = models.FileField(upload_to= subir_docscomite, null= True, blank= True)
+    subido_el = models.DateField(auto_now=False, auto_now_add=True, verbose_name="Subido el")
+    create_at = models.DateField(auto_now=False, auto_now_add=True, verbose_name="Creado el") 
+    modify_at = models.DateField(auto_now=True, auto_now_add=False, verbose_name="Actualizado el")
+
+    
+    class Meta:
+        verbose_name = "Nombre documento comite"
+        verbose_name_plural = "Nombre documentos comites"
         
     def __str__(self) -> str:
         return self.nombre_documento
