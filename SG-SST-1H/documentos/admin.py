d326a9460a259fb11905b0219e539d2dab1056dd
mao28rr <mauricio.restrepo@fundacionview.org>
2021-11-12 20:30:48 -0500

    actualizacion

--- SG-SST-1H/documentos/admin.py
+++ SG-SST-1H/documentos/admin.py
@@ -1,8 +1,41 @@
 from django.contrib import admin
 from .models import *
 
+# class para visualizar tipo de empresa
+class TipoDocumentoAdmin(admin.ModelAdmin):
+    reandoly_fields =('create_at', 'modify_at')
+    list_display = ('nombre_tipo', )
+    ordering = ('nombre_tipo', )
+    list_filter = ('nombre_tipo', )
+    search_fields = ('nombre_tipo', )
+
+class Meta:
+    model = (TipoDocumento)
+
+admin.site.register (TipoDocumento, TipoDocumentoAdmin)
+
+class DocuEmpresaAdmin(admin.ModelAdmin):
+    reandoly_fields =('create_at', 'modify_at')
+    list_display = ('empresa','nombre_documento',)
+    ordering = ('empresa', 'nombre_documento')
+    list_filter = ('nombre_documento', )
+    search_fields = ('nombre_documento',)
+class Meta:
+    model = (DocuEmpresa)
+
+admin.site.register (DocuEmpresa, DocuEmpresaAdmin)
 # Register your models here.
-admin.site.register (TipoDocumento)
-admin.site.register (DocuEmpresa)
+
+class DocuEmpleadosAdmin(admin.ModelAdmin):
+    reandoly_fields =('create_at', 'modify_at')
+    list_display = ('empresa','nombre_documento')
+    ordering = ('empresa', 'nombre_documento',)
+    list_filter = ('nombre_documento', )
+    search_fields = ('nombre_documento', )
+
+class Meta:
+    model = (DocuEmpresa, DocuEmpleadosAdmin)
+
+
 admin.site.register (DocuEmpleados)
 admin.site.register (DocsComite)
--- SG-SST-1H/riesgos/admin.py
+++ SG-SST-1H/riesgos/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import *
+
 
 # Register your models here.
+admin.site.register (Evaluacion)
+admin.site.register (TipoRiesgo)
+admin.site.register (AmenazaRiesgo)
+admin.site.register (ClasificacionesRiesgos)
+admin.site.register (CausaAmenaza)
+admin.site.register (OrigenAmenaza)
+admin.site.register (DetalleAmenaza)
