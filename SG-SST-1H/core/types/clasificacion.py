147c6df353b25577353a477db49d94f44e468642
Andres <andres.py@fundacionview.org>
2021-11-13 09:55:32 -0500

    actuliazacion andres

---
+++ SG-SST-1H/core/types/clasificacion.py
@@ -0,0 +1,4 @@
+Clasificacion=(
+    (("Inicial"),("Inicial")),
+    (("Prorroga"),("Prorroga"))
+)
--- SG-SST-1H/incapacidad/admin.py
+++ SG-SST-1H/incapacidad/admin.py
@@ -2,4 +2,13 @@ from django.contrib import admin
 from .models import * 
 
 # Register your models here.
-admin.site.register(Ausentismo)
+
+class AusentismoAdmin(admin.ModelAdmin):
+    readondly_fields = ('create_at, modify_at',)
+    list_display = ('nombre_completo', 'origen_incapacidad', 'codigo_enfermedad', 'clasificacion',)
+    ordering = ('mes', 'nombre_completo',)
+    list_filter = ('nombre_completo',)
+    search_field = ('nombre_completo',)
+   
+
+admin.site.register(Ausentismo, AusentismoAdmin )
